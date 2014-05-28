78721622f6d3c1eba958c03f8e552a52c5114d91
claudiosmweb <contato@claudiosmweb.com>
2014-05-27 22:49:16 -0300

    added initial files

---
+++ woocommerce-shortcodes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Shortcodes
+ * Plugin URI: https://www.woothemes.com/
+ * Description: WooCommerce Shortcodes.
+ * Version: 1.0.0
+ * Author: WooThemes, Claudio Sanches
+ * Author URI: http://woothemes.com
+ * Text Domain: woocommerce-shortcodes
+ * Domain Path: /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
